f3a14d6451bc70a4274c5e0f71410946e54370be
MarcioSill <marcio.sill@hotmail.com>
2022-09-02 17:51:26 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/marcioeduca/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.marcioeduca.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcioeduca.workshopmongo.domain.User;
+import com.marcioeduca.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
