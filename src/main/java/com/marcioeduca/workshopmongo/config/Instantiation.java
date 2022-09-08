391dfdd1823d1acdebf887b61017abe2815f18dd
MarcioSill <marcio.sill@hotmail.com>
2022-09-08 18:39:25 -0300

    Obtendo um usuário por id

--- src/main/java/com/marcioeduca/workshopmongo/config/Instantiation.java
+++ src/main/java/com/marcioeduca/workshopmongo/config/Instantiation.java
@@ -23,8 +23,9 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		User Joao = new User(null, "Joao Grog", "joao@gmail.com");
 		
-		userRepository.saveAll(Arrays.asList(maria, alex,bob));
+		userRepository.saveAll(Arrays.asList(maria, alex, bob, Joao));
 	}
 	
 }
--- src/main/java/com/marcioeduca/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/marcioeduca/workshopmongo/repository/UserRepository.java
@@ -7,5 +7,5 @@ import com.marcioeduca.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-	
+
 }
