59d0408b4fd06ab7d5afd84cfff307cfcde367d2
MarcioSill <marcio.sill@hotmail.com>
2022-09-26 14:42:56 -0300

    Correção

--- src/main/java/com/marcioeduca/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/marcioeduca/workshopmongo/repository/PostRepository.java
@@ -1,8 +1,11 @@
 package com.marcioeduca.workshopmongo.repository;
 
+
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -10,9 +13,12 @@ import com.marcioeduca.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
-	@Query("{ 'title': { $regex:?0, $options: 'i' } }")
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
-	
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	@Query("{$and: [ { date: {$gte:?1} }, { date: { $lte:?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullsearch(String text, Date minDate, Date maxDate);
+
 }
--- src/main/java/com/marcioeduca/workshopmongo/services/PostService.java
+++ src/main/java/com/marcioeduca/workshopmongo/services/PostService.java
@@ -1,6 +1,8 @@
 package com.marcioeduca.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,19 +13,21 @@ import com.marcioeduca.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class PostService {
-	
 	@Autowired
 	private PostRepository repo;
-		
+	
 	public Post findById(String id) {
-		Post user = repo.findById(id).get();
-		if (user == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado");
-		}
-		return user;
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
-	public List<Post> findByTitle(String text){
+	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
 	}
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullsearch(text, minDate, maxDate);
+	}
 }
+
